8b886a74c28aa4f314f0fd5b6696f6fc5f9e7b69
TL-P7 <seelesaki@gmail.com>
2024-01-27 18:55:19 +0800

    add lazygit plugin

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -41,4 +41,19 @@ return {
       })
     end,
   },
+  {
+    "kdheepak/lazygit.nvim",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "nvim-lua/plenary.nvim"
+    },
+    config = function()
+      require("telescope").load_extension("lazygit")
+      vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+        callback = function()
+          require('lazygit.utils').project_root_dir()
+        end
+      })
+    end,
+  },
 }
