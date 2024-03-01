3c7c620dc64793a5765b657032d586bb8fb4c892
TL-P7 <seelesaki@gmail.com>
2024-03-01 17:20:01 +0800

    add running options for python

--- .config/nvim/lua/plugins/competitest.lua
+++ .config/nvim/lua/plugins/competitest.lua
@@ -1,15 +1,22 @@
 return {
   'xeluxee/competitest.nvim',
   dependencies = 'MunifTanjim/nui.nvim',
-  ft = { 'cpp' },
+  ft = { 'cpp', 'python' },
   config = function()
     require('competitest').setup({
       compile_command = {
+        c         = { exec = "gcc", args = { "-Wall", "$(FNAME)", "-o", "$(FNOEXT)" } },
         cpp       = { exec = 'g++', args = { '$(FNAME)', '-std=c++17', '-O2', '-Wall', '-o', '$(FNOEXT)' } },
+        rust      = { exec = "rustc", args = { "$(FNAME)" } },
+        java      = { exec = "javac", args = { "$(FNAME)" } },
         some_lang = { exec = 'some_compiler', args = { '$(FNAME)' } },
       },
       run_command = {
+        c         = { exec = "./$(FNOEXT)" },
         cpp       = { exec = './$(FNOEXT)' },
+        rust      = { exec = "./$(FNOEXT)" },
+        python    = { exec = "python", args = { "$(FNAME)" } },
+        java      = { exec = "java", args = { "$(FNOEXT)" } },
         some_lang = { exec = 'some_interpreter', args = { '$(FNAME)' } },
       },
     })
