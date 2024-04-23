831a5c204af2f2c97db2dcff842e1d354769e2de
TL-P7 <seelesaki@gmail.com>
2024-04-23 21:33:06 +0800

    set up lsp

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -85,8 +85,13 @@ return {
             }
           })
           lspconfig['clangd'].setup({})
-          lspconfig['bashls'].setup({})
+          lspconfig['bashls'].setup({
+            enableSourceErrorDiagnostics = true,
+          })
           lspconfig['pyright'].setup({})
+          lspconfig['html'].setup({})
+          lspconfig['kotlin_language_server'].setup({})
+
           vim.api.nvim_create_autocmd('LspAttach', {
             desc = 'LSP actions',
             callback = function(event)
@@ -95,9 +100,9 @@ return {
               vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
               vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
               vim.keymap.set('n', 'gy', vim.lsp.buf.type_definition, opts)
-              vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+              vim.keymap.set('n', 'gR', vim.lsp.buf.references, opts)
               vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-              vim.keymap.set({ 'n', 'v' }, "<leader>fa", vim.lsp.buf.code_action, opts)
+              vim.keymap.set({ 'n', 'v' }, "<M-CR>", vim.lsp.buf.code_action, opts)
               vim.keymap.set('n', '[g', vim.diagnostic.goto_prev, opts)
               vim.keymap.set('n', ']g', vim.diagnostic.goto_next, opts)
               vim.keymap.set("n", "<leader>sn", vim.lsp.buf.signature_help, opts)
@@ -128,5 +133,13 @@ return {
         library = { plugins = { "nvim-dap-ui" }, types = true },
       })
     end
+  },
+  {
+    "folke/trouble.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+      position = "right",
+      use_diagnostic_signs = true,
+    },
   }
 }
