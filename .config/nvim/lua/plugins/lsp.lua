357309bccb6cbfcbcae773821dcb22b7b3454359
TL-P7 <seelesaki@gmail.com>
2024-11-06 23:13:12 +0800

    add fish gitignore

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -3,18 +3,18 @@ return {
     'nvim-treesitter/nvim-treesitter',
     lazy = false,
     priority = 1000,
-    build = ":TSUpdate",
+    build = ':TSUpdate',
     config = function()
-      require("nvim-treesitter.configs").setup({
+      require('nvim-treesitter.configs').setup({
         ensure_installed = {
-          "c",
-          "cpp",
-          "bash",
-          "go",
-          "lua",
-          "vim",
-          "python",
-          "json"
+          'c',
+          'cpp',
+          'bash',
+          'go',
+          'lua',
+          'vim',
+          'python',
+          'json'
         },
         auto_install = true,
         highlight = {
@@ -27,10 +27,10 @@ return {
         incremental_selection = {
           enable = true,
           keymaps = {
-            init_selection    = "<CR>",
-            node_incremental  = "<CR>",
-            node_decremental  = "<BS>",
-            scope_incremental = "<TAB>",
+            init_selection    = '<CR>',
+            node_incremental  = '<CR>',
+            node_decremental  = '<BS>',
+            scope_incremental = '<TAB>',
           },
         }
       })
@@ -38,31 +38,31 @@ return {
   },
 
   {
-    "VonHeikemen/lsp-zero.nvim",
+    'VonHeikemen/lsp-zero.nvim',
     lazy = false,
-    branch = "v3.x",
+    branch = 'v3.x',
     dependencies = {
       {
-        "williamboman/mason-lspconfig.nvim",
+        'williamboman/mason-lspconfig.nvim',
         dependencies = {
-          { "williamboman/mason.nvim", build = ":MasonUpdate", },
+          { 'williamboman/mason.nvim', build = ':MasonUpdate', },
         },
         config = function()
-          require("mason").setup({
+          require('mason').setup({
             ui = {
               icons = {
-                package_installed = "✓",
-                package_pending = "➜",
-                package_uninstalled = "✗"
+                package_installed = '✓',
+                package_pending = '➜',
+                package_uninstalled = '✗'
               }
             }
           })
-          require("mason-lspconfig").setup({
+          require('mason-lspconfig').setup({
             ensure_installed = {
-              "lua_ls",
-              "clangd",
-              "bashls",
-              "pyright",
+              'lua_ls',
+              'clangd',
+              'bashls',
+              'pyright',
             },
             automatic_installation = true,
           })
@@ -70,37 +70,74 @@ return {
       },
 
       {
-        "neovim/nvim-lspconfig",
+        'neovim/nvim-lspconfig',
         lazy = false,
         config = function()
-          require("lsp-zero").extend_lspconfig()
-          require("neodev").setup()
-          local lspconfig = require("lspconfig")
+          require('lsp-zero').extend_lspconfig()
+          require('neodev').setup()
+          local lspconfig = require('lspconfig')
           lspconfig['lua_ls'].setup({
             settings = {
               Lua = {
+                hint = {
+                  enable = true,
+                },
                 completion = {
-                  callSnippet = "Replace"
+                  callSnippet = 'Replace'
                 }
               }
             }
           })
           lspconfig['clangd'].setup({
-            cmd = { "clangd", "--clang-tidy", "--background-index", "--all-scopes-completion" },
+            cmd = { 'clangd', '--clang-tidy', '--background-index', '--all-scopes-completion' },
+            settings = {
+              clangd = {
+                InlayHints = {
+                  Designators = true,
+                  Enabled = true,
+                  ParameterNames = true,
+                  DeducedTypes = true,
+                },
+                fallbackFlags = { "-std=c++20" },
+              },
+            }
           })
+          lspconfig['ts_ls'].setup({})
           lspconfig['bashls'].setup({
             enableSourceErrorDiagnostics = true,
           })
-          lspconfig['pyright'].setup({})
+          lspconfig['pyright'].setup({
+            settings = {
+              python = {
+                analysis = {
+                  inlayHints = {
+                    variableTypes = true,       -- 显示变量类型的 inlay hints
+                    functionReturnTypes = true, -- 显示函数返回类型的 inlay hints
+                  },
+                },
+              },
+            }
+          })
           lspconfig['html'].setup({})
           lspconfig['kotlin_language_server'].setup({
-            cmd = { "kotlin-language-server" },
-            filetypes = { "kotlin" },
+            cmd = { 'kotlin-language-server' },
+            filetypes = { 'kotlin' },
             root_dir = function(fname)
-              return lspconfig.util.root_pattern("settings.gradle.kts", "build.gradle.kts", "pom.xml")(fname) or
+              return lspconfig.util.root_pattern('settings.gradle.kts', 'build.gradle.kts', 'pom.xml')(fname) or
                   lspconfig.util.find_git_ancestor(fname) or lspconfig.util.path.dirname(fname)
             end
           })
+          lspconfig['markdown_oxide'].setup({})
+          lspconfig['tinymist'].setup({
+            single_file_support = true,
+            on_attach = function(client)
+              client.server_capabilities.semanticTokensProvider = nil
+            end,
+          })
+
+          vim.keymap.set("n", "<leader>th", function()
+            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled(), nil)
+          end, { desc = "Toggle inlay hints" })
 
           vim.api.nvim_create_autocmd('BufEnter', {
             desc = 'LSP actions',
@@ -112,79 +149,70 @@ return {
               vim.keymap.set('n', 'gy', vim.lsp.buf.type_definition, opts)
               vim.keymap.set('n', 'gR', vim.lsp.buf.references, opts)
               vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-              vim.keymap.set({ 'n', 'v' }, "<M-CR>", vim.lsp.buf.code_action, opts)
+              vim.keymap.set({ 'n', 'v' }, '<M-CR>', vim.lsp.buf.code_action, opts)
               vim.keymap.set('n', '[g', vim.diagnostic.goto_prev, opts)
               vim.keymap.set('n', ']g', vim.diagnostic.goto_next, opts)
-              vim.keymap.set("n", "<leader>sn", vim.lsp.buf.signature_help, opts)
+              vim.keymap.set('n', '<leader>sn', vim.lsp.buf.signature_help, opts)
               vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
               vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
               vim.keymap.set('n', '<space>wl', function()
                 print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
               end, opts)
-              vim.keymap.set({ 'n', 'x' }, '<C-f>', "<Cmd>GuardFmt<CR>", opts)
+              vim.keymap.set({ 'n', 'x' }, '<C-f>', '<Cmd>Guard fmt<CR>', opts)
             end
           })
         end,
       }
     },
     config = function()
-      local lsp = require("lsp-zero")
-      lsp.on_attach(function(client, bufnr)
+      local lsp = require('lsp-zero')
+      lsp.on_attach(function(_, bufnr)
         lsp.default_keymaps({ buffer = bufnr })
       end)
       lsp.setup()
     end,
   },
   {
-    "folke/neodev.nvim",
+    'folke/neodev.nvim',
     lazy = false,
     config = function()
-      require("neodev").setup({
-        library = { plugins = { "nvim-dap-ui" }, types = true },
+      require('neodev').setup({
+        library = { plugins = { 'nvim-dap-ui' }, types = true },
       })
     end
   },
   {
-    "folke/trouble.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
+    'folke/trouble.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
     opts = {
-      position = "right",
+      position = 'right',
       use_diagnostic_signs = true,
     },
   },
   {
-    "nvimdev/guard.nvim",
+    'nvimdev/guard.nvim',
     config = function()
-      local ft = require("guard.filetype")
-      ft("c"):fmt("clang-format")
-      ft("sh"):fmt({
-        cmd = "shfmt",
-        args = { "-i", "4" },
+      local ft = require('guard.filetype')
+      ft('c'):fmt('clang-format')
+      ft('sh'):fmt({
+        cmd = 'shfmt',
+        args = { '-i', '4' },
       })
-      ft("python"):fmt("black")
-      ft("javascript,json,markdown"):fmt("prettier")
-      ft("*^cpp"):lint("codespell")
-      -- Assuming you have guard-collection
-      -- ft('lang'):fmt('format-tool-1')
-      --     :append('format-tool-2')
-      --     :env(env_table)
-      --     :lint('lint-tool-1')
-      --     :extra(extra_args)
+      ft('python'):fmt('black')
+      ft('javascript,json,markdown'):fmt('prettier')
+      -- ft('typst'):fmt('typstfmt')
+      -- ft('*'):lint('codespell')
 
-      -- Call setup() LAST!
-      require('guard').setup({
-        -- Choose to format on every write to a buffer
+      vim.g.guard_config = {
         fmt_on_save = false,
-        -- Use lsp if no formatter was defined for this filetype
         lsp_as_default_formatter = true,
-        -- By default, Guard writes the buffer on every format
-        -- You can disable this by setting:
-        -- save_on_fmt = false,
-      })
+        save_on_format = true
+      }
     end,
     -- Builtin configuration, optional
     dependencies = {
-      "nvimdev/guard-collection",
+      'nvimdev/guard-collection',
+      'VonHeikemen/lsp-zero.nvim'
     },
   },
 }
