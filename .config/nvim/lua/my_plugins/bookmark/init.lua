df2b161c27d47023a0dc74b6d2f00606ce2a4bfe
TL-P7 <seelesaki@gmail.com>
2024-11-06 23:13:52 +0800

    modify nvim bookmark

--- .config/nvim/lua/my_plugins/bookmark/init.lua
+++ .config/nvim/lua/my_plugins/bookmark/init.lua
@@ -2,7 +2,7 @@ local function open_path()
   local home = vim.fn.getenv('HOME')
   local paths = {
     "1. " .. home .. "/.dotfiles/.config/nvim/",
-    "2. " .. home .. "/Nutstore Files/code/cp/",
+    "2. " .. home .. "/Nutstore Files/code/Competitive Programming/",
     "3. " .. home .. "/Nutstore Files/code/notes/",
     "4. " .. home .. "/.dotfiles/.config/"
   }
@@ -17,11 +17,16 @@ local function open_path()
     index = tonumber(input)
   end)
 
-  if index > 0 and index <= #paths then
-    require("telescope.builtin").find_files({ cwd = string.sub(paths[index], 4, #paths[index]), hidden = true })
-  else
-    print("Invalid selection")
+  if not index then
+    return
   end
+
+  if index <= 0 or index > #paths then
+    print("Invalid Selection")
+    return
+  end
+
+  require("telescope.builtin").find_files({ cwd = string.sub(paths[index], 4, #paths[index]), hidden = true })
 end
 
 vim.keymap.set("n", "<leader>mb", open_path, { silent = true, noremap = true, desc = "bookmark" })
