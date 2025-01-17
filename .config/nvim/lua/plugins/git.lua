3634184c90d4a3fa146d3cd06e8d568c4fa78b6b
TL-P7 <seelesaki@gmail.com>
2025-01-17 11:57:35 +0800

    feat(nvim): add keybinding for git hunk

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -2,8 +2,10 @@ return {
   {
     "lewis6991/gitsigns.nvim",
     event = "BufEnter",
-    config = function ()
+    config = function()
       require("gitsigns").setup()
+      vim.keymap.set("n", "]s", "<Cmd>lua require('gitsigns').nav_hunk('next')<CR>", { noremap = true, silent = true })
+      vim.keymap.set("n", "[s", "<Cmd>lua require('gitsigns').nav_hunk('prev')<CR>", { noremap = true, silent = true })
     end
   }
 }
