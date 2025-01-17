8393f7c98f0f9b83d5573d119cd59bdad248b6bb
TL-P7 <seelesaki@gmail.com>
2025-01-17 11:57:16 +0800

    feat(nvim): replace joshuto with yazi

--- .config/nvim/lua/plugins/filebrowser.lua
+++ .config/nvim/lua/plugins/filebrowser.lua
@@ -90,8 +90,40 @@ return {
       })
     end
   },
+
+  ---@type LazySpec
   {
-    "theniceboy/joshuto.nvim",
-    lazy = "BufEnter",
+    "mikavilpas/yazi.nvim",
+    event = "VeryLazy",
+    keys = {
+      -- 👇 in this section, choose your own keymappings!
+      {
+        "<leader>mt",
+        mode = { "n", "v" },
+        "<Cmd>Yazi<CR>",
+        desc = "Open yazi at the current file",
+      },
+      {
+        -- Open in the current working directory
+        "<leader>mw",
+        "<Cmd>Yazi cwd<Cr>",
+        desc = "Open the file manager in nvim's working directory",
+      },
+      {
+        -- NOTE: this requires a version of yazi that includes
+        -- https://github.com/sxyazi/yazi/pull/1305 from 2024-07-18
+        '<leader>ms',
+        "<Cmd>Yazi toggle<Cr>",
+        desc = "Resume the last yazi session",
+      },
+    },
+    ---@type YaziConfig
+    opts = {
+      -- if you want to open yazi instead of netrw, see below for more info
+      open_for_directories = false,
+      keymaps = {
+        show_help = '<f1>',
+      },
+    },
   }
 }
