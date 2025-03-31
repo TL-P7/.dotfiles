975b2111b885b93a2ee72f78fda2e2903b3a5b12
TL-P7 <seelesaki@gmail.com>
2025-03-31 15:13:03 +0800

    feat(nvim): add run command for rust

--- .config/nvim/lua/my_plugins/compile_run/init.lua
+++ .config/nvim/lua/my_plugins/compile_run/init.lua
@@ -9,43 +9,44 @@ end
 
 local compileRun = function()
   local ft = vim.bo.filetype
-  vim.cmd("w")
-  if ft == "cpp" then
+  vim.cmd('w')
+  if ft == 'cpp' then
     split()
-    vim.cmd("term g++ % -o %< -std=c++23 -lfmt -g -Wall -Wextra -Wpedantic && time ./%<")
-  elseif ft == "c" then
+    vim.cmd('term g++ % -o %< -std=c++23 -lfmt -g -Wall -Wextra -Wpedantic && time ./%<')
+  elseif ft == 'c' then
     split()
-    if vim.fn.filereadable("Makefile") == 1 then
-      vim.cmd("term make && ./%")
+    if vim.fn.filereadable('Makefile') == 1 then
+      vim.cmd('term make && ./%')
     else
-      vim.cmd("term gcc -std=c99 % -o %< && time ./%<")
+      vim.cmd('term gcc -std=c99 % -o %< && time ./%<')
     end
-  elseif ft == "python" then
+  elseif ft == 'python' then
     split()
-    vim.cmd("term time python3 %")
-  elseif ft == "sh" then
+    vim.cmd('term time python3 %')
+  elseif ft == 'sh' then
     split()
-    vim.cmd("term time bash %")
-  elseif ft == "markdown" then
-    vim.cmd("MarkdownPreview")
-  elseif ft == "tex" then
-    require("knap").process_once()
-  elseif ft == "lua" then
+    vim.cmd('term time bash %')
+  elseif ft == 'markdown' then
+    vim.cmd('MarkdownPreview')
+  elseif ft == 'tex' then
+    require('knap').process_once()
+  elseif ft == 'lua' then
     split()
-    vim.cmd("term lua %")
-  elseif ft == "go" then
+    vim.cmd('term lua %')
+  elseif ft == 'go' then
     split()
-    vim.cmd("term go run %")
-  elseif ft == "kotlin" then
+    vim.cmd('term go run %')
+  elseif ft == 'kotlin' then
     split()
-    vim.cmd("term kotlinc % -include-runtime -d %<.jar && time java -jar %<.jar")
-  elseif ft == "html" then
-    vim.cmd("silent !firefox % &")
-  else
-    if ft == "typst" then
-      vim.cmd('TypstPreview')
-    end
+    vim.cmd('term kotlinc % -include-runtime -d %<.jar && time java -jar %<.jar')
+  elseif ft == 'html' then
+    vim.cmd('silent !firefox % &')
+  elseif ft == 'typst' then
+    vim.cmd('TypstPreview')
+  elseif ft == 'rust' then
+    split()
+    vim.cmd('term cargo run')
   end
 end
 
-vim.keymap.set("n", "<leader>rr", compileRun, { silent = true, noremap = true })
+vim.keymap.set('n', '<leader>rr', compileRun, { silent = true, noremap = true })
